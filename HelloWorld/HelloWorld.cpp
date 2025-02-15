9e4b16963e35cf9a5d73bdbac380a703ef9b6a97
XianghuWang-287 <wangxianghu1019@126.com>
2025-02-15 01:43:28 -0800

    implement the LVN

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -20,6 +20,8 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/FormatVariadic.h"
+#include <map>
 
 using namespace llvm;
 
@@ -30,10 +32,85 @@ using namespace llvm;
 // everything in an anonymous namespace.
 namespace {
 
+struct Expression {
+  unsigned Opcode;
+  unsigned LHS, RHS;
+
+  Expression(unsigned Op, unsigned L, unsigned R) : Opcode(Op), LHS(L), RHS(R) {
+    // Handle commutativity for addition and multiplication
+    if (Op == Instruction::Add || Op == Instruction::Mul) {
+      if (L > R) std::swap(LHS, RHS);
+    }
+  }
+
+  bool operator<(const Expression &Other) const {
+    return std::tie(Opcode, LHS, RHS) < std::tie(Other.Opcode, Other.LHS, Other.RHS);
+  }
+};
+
+std::string getOpcodeName(unsigned Opcode) {
+  switch (Opcode) {
+  case Instruction::Add: return "add";
+  case Instruction::Sub: return "sub";
+  case Instruction::Mul: return "mul";
+  case Instruction::UDiv:
+  case Instruction::SDiv: return "div";
+  default: return "unknown";
+  }
+}
+
+
 // This method implements what the pass does
 void visitor(Function &F) {
-    errs() << "(llvm-tutor) Hello from: "<< F.getName() << "\n";
-    errs() << "(llvm-tutor)   number of arguments: " << F.arg_size() << "\n";
+  errs() << "ValueNumbering: " << F.getName() << "\n";
+
+  std::map<Expression, unsigned> LVNTable;
+  std::map<Value *, unsigned> ValueNumbers;
+  unsigned nextValueNumber = 1;
+
+  for (auto &BB : F) {
+    for (auto &I : BB) {
+      //handle store operation
+      if (auto *Store = dyn_cast<StoreInst>(&I)) {
+        Value *StoredValue = Store->getValueOperand(); //value being stored
+        Value *Pointer = Store->getPointerOperand(); //Destination where it's stored
+
+        if (!ValueNumbers.count(StoredValue)) {
+          ValueNumbers[StoredValue] = nextValueNumber++; //If StoredValue is new, assign a fresh number
+        }
+        ValueNumbers[Pointer] = ValueNumbers[StoredValue]; //Maps Pointer to the stored value’s number
+        //errs() << I << "\t" << ValueNumbers[Pointer] << " = " << ValueNumbers[StoredValue] << "\n";
+        errs() << formatv("{0,-40} {1} = {2}\n", I, ValueNumbers[Pointer], ValueNumbers[StoredValue]);
+      } else if (auto *Load = dyn_cast<LoadInst>(&I)) { //handle load operation
+        Value *Pointer = Load->getPointerOperand();
+
+        if (ValueNumbers.count(Pointer)) {
+          ValueNumbers[&I] = ValueNumbers[Pointer]; //If Pointer is known, assigns same number
+        } else {
+          ValueNumbers[&I] = nextValueNumber++; //Otherwise, assigns a fresh number
+        }
+        //errs() << I << "\t" << ValueNumbers[&I] << " = " << ValueNumbers[Pointer] << "\n";
+        errs() << formatv("{0,-40} {1} = {2}\n", I, ValueNumbers[&I], ValueNumbers[Pointer]);
+      } else if (auto *BinOp = dyn_cast<BinaryOperator>(&I)) {  //handle arithmetic operation
+        unsigned LHS = ValueNumbers[BinOp->getOperand(0)]; //Get value numbers for operands
+        unsigned RHS = ValueNumbers[BinOp->getOperand(1)];
+        Expression Expr(BinOp->getOpcode(), LHS, RHS); //Creates a unique key to detect redundancy
+
+        if (LVNTable.count(Expr)) { //If expression exists, reuse its value number (redundant label)
+          ValueNumbers[&I] = LVNTable[Expr];
+          //errs() << I << "\t" << ValueNumbers[&I] << " = "
+                 //<< LHS << " " << getOpcodeName(BinOp->getOpcode()) << " " << RHS << " (redundant)\n";
+          errs() << formatv("{0,-40} {1} = {2} {3} {4} (redundant)\n", I, ValueNumbers[&I], LHS, getOpcodeName(BinOp->getOpcode()), RHS);
+        } else { //Otherwise, assigns a fresh number and stores it
+          ValueNumbers[&I] = nextValueNumber++;
+          LVNTable[Expr] = ValueNumbers[&I];
+          //errs() << I << "\t" << ValueNumbers[&I] << " = "
+                 //<< LHS << " " << getOpcodeName(BinOp->getOpcode()) << " " << RHS << "\n";
+          errs() << formatv("{0,-40} {1} = {2} {3} {4}\n", I, ValueNumbers[&I], LHS, getOpcodeName(BinOp->getOpcode()), RHS);
+        }
+      }
+    }
+  }
 }
 
 // New PM implementation
